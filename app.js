d1939267261c340755f0f54d30e7c2660aceed02
Javoh1rolimboyev <javohirolimov306@gmail.com>
2026-03-25 09:45:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function reverseText() {
+    let text = document.getElementById("textInput").value;
+
+    let reversed = text.split("").reverse().join("");
+
+    document.getElementById("result").textContent = reversed;
+}
